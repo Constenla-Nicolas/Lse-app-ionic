74eeae999fd372db3c603eb41f166a8c643a067e
consti <constinic@gmail.com>
2021-09-11 22:21:06 -0300

    aoeu

---
+++ src/pantallas/Colores.vue
@@ -0,0 +1,48 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <ion-slides pager="true" :options="slideOpts">
+                <ion-slide>
+                    <h1>Slide 1</h1>
+                </ion-slide>
+                <ion-slide>
+                    <h1>Slide 2</h1>
+                </ion-slide>
+                <ion-slide>
+                    <h1>Slide 3</h1>
+                </ion-slide>
+            </ion-slides>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {IonPage, IonContent, IonSlides, IonSlide} from "@ionic/vue";
+
+import 'swiper/swiper-bundle.min.css';
+import '@ionic/vue/css/ionic-swiper.css';
+
+export default {
+    components:{
+        IonPage,
+        IonContent,
+        IonSlides,
+        IonSlide
+        
+    },
+    setup() {
+    // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+    return { slideOpts }
+  }
+};
+ 
+</script>
+
+<style scoped>
+
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import Bienvenido from '../pantallas/Bienvenido.vue';
 import EleccionQueHacer from '../pantallas/EleccionQueHacer.vue';
 import EleccionQueAprender from "../pantallas/EleccionQueAprender.vue"
-import Abecedario from "../pantallas/pantallas_aprender/Abecedario.vue"
-import Numeros from "../pantallas/pantallas_aprender/Numeros.vue"
-import Colores from "../pantallas/pantallas_aprender/Colores.vue"
-import Acciones from "../pantallas/pantallas_aprender/Acciones.vue"
-import Conceptos from "../pantallas/pantallas_aprender/Conceptos.vue"
+import Abecedario from "../pantallas/Abecedario.vue"
+import Numeros from "../pantallas/Numeros.vue"
+import Colores from "../pantallas/Colores.vue"
+import Acciones from "../pantallas/Acciones.vue"
+import Conceptos from "../pantallas/Conceptos.vue"
 
 
 const routes = [
