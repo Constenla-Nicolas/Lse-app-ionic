d6b33f167721bea2c3264994a42c87d42e320f75
ArielDAngelo <49006142+ArielDAngelo@users.noreply.github.com>
2021-09-12 19:59:47 -0300

    mejorando colores

--- src/pantallas/Colores.vue
+++ src/pantallas/Colores.vue
@@ -1,88 +1,98 @@
 <template>
-    <ion-page>
-        <ion-content>
-            <ion-slides ref="slides">
-              <ion-slide>
-                <img id="imagen" src="../imagenes/logo_icono.png">
-                <ion-p id="significa">SIGNIFICA</ion-p>
-                
-                <div id="significado">
-                      <ion-p id="nombreColor">AMARILLO</ion-p>
-                      <ion-p id="definicion">Color como el del oro, la banana, el sol, la yema de huevo. Es el tercer color del espectro solar.</ion-p>
-                </div>
-
-              </ion-slide>
-              <ion-slide>
-                <img src="../imagenes/logo_icono.png">
-              </ion-slide>
-            </ion-slides>
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-content>
+      <ion-slides pager="true" :options="slideOpts">
+        <ion-slide>
+            <img id="imagen" src="../imagenes/logo_icono.png">
+            <ion-p id="significa">SIGNIFICA</ion-p>    
+            <div id="significado">
+              <ion-p id="nombreColor">AMARILLO</ion-p>
+              <ion-p id="definicion">Color como el del oro, la banana, el sol, la yema de huevo. Es el tercer color del espectro solar.</ion-p>
+            </div>
+          </ion-slide>
+          <ion-slide>
+            <img id="imagen" src="../imagenes/logo_icono.png">
+            <ion-p id="significa">SIGNIFICA</ion-p>    
+            <div id="significado">
+              <ion-p id="nombreColor">AMARILLO</ion-p>
+              <ion-p id="definicion">Color como el del oro, la banana, el sol, la yema de huevo. Es el tercer color del espectro solar.</ion-p>
+            </div>
+          </ion-slide>
+          
+        </ion-slides>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import {IonPage, IonContent, IonSlides, IonSlide} from "@ionic/vue";
+  import {IonPage, IonContent, IonSlides, IonSlide} from "@ionic/vue";
 
-
-export default {
-    components:{
-        IonPage,
-        IonContent,
-        IonSlide,
-        IonSlides,        
-    },
-    methods: {    
-      goTo(slideIndex) {      
-        // permite arrastrar la pantalla
-        this.$refs.slides.slideTo(slideIndex);
-      }
-    },
-}
+  export default {
+      components:{
+          IonPage,
+          IonContent,
+          IonSlide,
+          IonSlides,        
+      },
+      setup() {
+    // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+    const slideOpts = {
+      initialSlide: 0,
+      speed: 400
+    };
+    return { slideOpts }
+  }
+  }
 </script>
 
 <style scoped>
-#imagen{
-    border: yellow;
-    width: 248px;
-    height: 257px;
+  ion-slide{
     display: block;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    margin-top: 12%;
-    margin-bottom: 12px;
-}
-#significa{
-    color: #E7E40D;
-    text-transform:unset;
-    font-size: 36px;
-    width: 246px;
-    height: 70px;
-    letter-spacing: 0px;
-    margin-bottom: 12px; 
-}
-#significado{
-    background-color: #E7E40D;
-    width: 309px;
-    height: 187px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-}
-#nombreColor{
-    color: #553071;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    margin-bottom: 12px;
-}
-#definicion{
-    color: #553071;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-}
+    width: 500px;
+    height: 2000px;
+  }
+  #imagen{
+      border: yellow;
+      width: 248px;
+      height: 257px;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+      margin-top: 12%;
+      margin-bottom: 12px;
+  }
+  #significa{
+      color: #E7E40D;
+      text-transform:unset;
+      font-size: 36px;
+      width: 246px;
+      height: 70px;
+      letter-spacing: 0px;
+      margin-bottom:20px; 
+  }
+  #significado{
+      background-color: #E7E40D;
+      width: 309px;
+      height: 187px;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+  }
+  #nombreColor{
+      color: #553071;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+      margin-bottom: 12px;
+  }
+  #definicion{
+      color: #553071;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+  }
 </style>
